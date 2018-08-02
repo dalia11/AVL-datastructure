ecfa5776d4f2263c76bda182e1df9ebbbb2534c3
dalia11 <dalia.talaat11@gmail.com>
2018-08-02 12:59:59 +0200

    AVL implementation

---
+++ src/avl/AvlNode.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avl;
+
+/**
+ *
+ * @author RS
+ */
+ class AvlNode<AnyType>
+    {
+            // Constructors
+        AvlNode( AnyType theElement )
+        {
+            this( theElement, null, null );
+        }
+
+        AvlNode( AnyType theElement, AvlNode<AnyType> lt, AvlNode<AnyType> rt )
+        {
+            element  = theElement;
+            left     = lt;
+            right    = rt;
+            height   = 0;
+        }
+
+        AnyType           element;      // The data in the node
+        AvlNode<AnyType>  left;         // Left child
+        AvlNode<AnyType>  right;        // Right child
+        int               height;       // Height
+    }
